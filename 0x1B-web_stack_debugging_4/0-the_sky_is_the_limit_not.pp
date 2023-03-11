2eb34addbfb179854ca8c72a4f370eb3750604fa
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-03-11 21:44:29 +0100

    FIxed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Puppet File to fix the Nginx Bug
 
 exec { 'increase limit':
-  command  => "sed -i 's/15/4096/' /etc/default/nginx",
-  path     => '/usr/bin/:/usr/local/bin/:/bin/'
+  command => "sed -i 's/15/4096/' /etc/default/nginx",
+  path    => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 exec {'restart nginx':
   command => 'sudo service nginx restart',
